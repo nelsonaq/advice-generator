e86f0dc0039a7e8fc8036d0a29cef22f692cb4ad
nelsonaq <chibuds217@gmail.com>
2022-04-03 04:34:53 +0800

    added documentation

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+"use-strict";
+
 const dice = document.querySelector(".dice");
 const advice = document.querySelector(".advice");
 const adviceId = document.querySelector(".advice-id");
 
+/**
+ * Generates random number between the given parameters
+ * @param {number} min - Minimum number
+ * @param {number} max - Maximum number
+ * @returns {number} Returns the random number
+ */
 const randomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
+/**
+ * Renders the id and advice on the DOM
+ * @param {object} obj - The object coming from the Advice Slip API
+ * @returns {undefined} - Returns when given "obj" argument is undefined
+ */
 const renderUI = function (obj) {
   //* Guard Clause
   if (!obj) return;
@@ -14,6 +27,10 @@ const renderUI = function (obj) {
   advice.textContent = obj.advice;
 };
 
+/**
+ * Generates advice coming from the Advice Slip API
+ * @returns {undefined} - Returns when the fetch is unsuccessful.
+ */
 const generateAdvice = async function () {
   const response = await fetch(
     `https://api.adviceslip.com/advice/${randomNumber(1, 224)}`
