c7c3def7b4ca6943d52a407e4ec1b1fd95b389f7
nelsonaq <chibuds217@gmail.com>
2022-04-03 02:23:47 +0800

    change dice to a button element, added random number functionality

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const dice = document.querySelector(".dice");
 
+const randomNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
 const fetchAdv = async function (num) {
   const response = await fetch(`https://api.adviceslip.com/advice/${num}`);
   const data = await response.json();
-  console.log(data);
+  return data;
 };
 
-const renderAdv = function () {
-  fetchAdv(21);
+const renderAdv = async function () {
+  const randomNum = randomNumber(1, 224);
+  console.log(randomNum);
+  const advSlip = await fetchAdv(randomNum);
+  const advObj = advSlip.slip;
+  console.log(advObj);
 };
 
 dice.addEventListener("click", renderAdv);
